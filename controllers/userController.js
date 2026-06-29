bf0c1bfea46fd7846277b77ad8926c8830792bf6
Illia Kolmykov <zabyz1337@gmail.com>
2026-06-29 10:01:37 +0200

    Add profile update

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,13 +21,49 @@ const searchUsers = async (req, res) => {
 
     res.json(users);
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateProfile = async (req, res) => {
+  try {
+    const { fullName, username, bio, avatar } = req.body;
+
+    if (username) {
+      const existingUser = await User.findOne({ username });
+
+      if (
+        existingUser &&
+        existingUser._id.toString() !== req.user._id.toString()
+      ) {
+        return res.status(400).json({
+          message: "Username already exists",
+        });
+      }
+    }
+
+    const user = await User.findById(req.user._id);
+
+    if (fullName !== undefined) user.fullName = fullName;
+    if (username !== undefined) user.username = username;
+    if (bio !== undefined) user.bio = bio;
+    if (avatar !== undefined) user.avatar = avatar;
+
+    await user.save();
+
+    const updatedUser = await User.findById(user._id).select("-password");
+
+    res.json({
+      message: "Profile updated successfully",
+      user: updatedUser,
     });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
 module.exports = {
   getProfile,
   searchUsers,
+  updateProfile,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
+
 const {
   getProfile,
   searchUsers,
+  updateProfile,
 } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/me", authMiddleware, getProfile);
+router.put("/me", authMiddleware, updateProfile);
 router.get("/search", authMiddleware, searchUsers);
 
 module.exports = router;
